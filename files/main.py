ce726a4b3af94ba1a0980b7bfa38153b5dc29022
Yousef Bakhsh <yousef.bakhsh@gmail.com>
2023-10-31 21:41:33 +0300

    created main file

---
+++ files/main.py
@@ -0,0 +1,7 @@
+# networkx is installed using pip in a virtual environment
+# networkx is an external library that helps with the creation of Graphs
+import networkx as nx
+
+# the domain is a university 
+# create a graph called university
+university = nx.Graph()
